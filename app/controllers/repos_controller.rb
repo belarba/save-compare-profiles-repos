69807fbecd720190df4c54a131691c23e9097224
Daniel Belarmino <belarba@gmail.com>
2021-04-17 13:34:49 +0100

    saving a specific repo and redirect to root

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,7 +1,23 @@
 class ReposController < ApplicationController
   def create
-    @repo = Repo.create(params[:repo_data].to_enum.to_h)
+    user_info = params[:repo_data]['owner']
+
+    user = User.find(user_info['id'].to_i)
+
+    user = User.create(user_info.to_enum.to_h) if user.blank?
+
+    repo_info = params[:repo_data].to_enum.to_h
+
+    repo_info['license'].present? ? license_name = repo_info['license']['name'] : nil
+
+    repo_info.delete('license')
+    repo_info.delete('owner')
+
+    repo_info.merge!(user: user)
+    repo_info.merge!(license_name: license_name)
+
+    @repo = Repo.create(repo_info)
     @repo.save
-    return RedirectToAction("index", "user");
+    redirect_to controller: :users, action: :index
   end
 end
