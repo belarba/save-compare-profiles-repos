c91410c9575e3538d6e47d59ce030994b6e1659b
Daniel Belarmino <belarba@gmail.com>
2021-04-17 19:58:04 +0100

    adjusted the view to correct display info if no user is found

--- app/controllers/fetch_repos_controller.rb
+++ app/controllers/fetch_repos_controller.rb
@@ -6,6 +6,7 @@ class FetchReposController < ApplicationController
     base_url = Repo.api_path
     get_url = base_url + params[:user_name] + '/repos'
 		response = RestClient.get(get_url){|response, request, result| response }
+    @code = response.code
 		@repos = JSON.parse(response)
   end
 end
--- app/controllers/fetch_users_controller.rb
+++ app/controllers/fetch_users_controller.rb
@@ -6,6 +6,7 @@ class FetchUsersController < ApplicationController
     base_url = User.api_path
     get_url = base_url + params[:user_name]
 		response = RestClient.get(get_url){|response, request, result| response }
+    @code = response.code
 		@repos = JSON.parse(response)
   end
 end
